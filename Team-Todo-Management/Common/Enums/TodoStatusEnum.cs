f1882b67aa429ded69491d20e70d5109058a054b
tortoise10h <tortoise10h@gmail.com>
2020-11-16 15:05:14 +0700

    [Source] base source

---
+++ Team-Todo-Management/Common/Enums/TodoStatusEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_Todo_Management.Common.Enums
+{
+    public enum TodoStatusEnum
+    {
+        New = 1,
+        Processing = 2,
+        Done = 3,
+        OverDue = 4
+    }
+}
---
+++ Team-Todo-Management/Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_Todo_Management.Models
+{
+    public class BaseModel
+    {
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
