5cff5fe169f665ee458a54f87ee5206d6e16ac80
tortoise10h <tortoise10h@gmail.com>
2020-11-16 15:05:14 +0700

    [Source] base source

---
+++ Team-Todo-Management/Common/Enums/TodoScopeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_Todo_Management.Common.Enums
+{
+    public enum TodoScopeEnum
+    {
+        Public = 1,
+        Private = 2
+    }
+}
---
+++ Team-Todo-Management/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Team_Todo_Management.Common.Enums;
+
+namespace Team_Todo_Management.Models
+{
+    public class Todo: BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TodoStatusEnum Status { get; set; }
+        public TodoScopeEnum Scope { get; set; }
+    }
+}
