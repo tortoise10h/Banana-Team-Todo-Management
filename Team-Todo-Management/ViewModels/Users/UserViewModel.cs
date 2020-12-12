e17ef246bf021a5372b6db273801ebb0bd0dce43
Nguyen Anh Khoa <nahntvt@gmail.com>
2020-12-12 13:07:25 +0700

    [Feature] list user and create user

--- Team-Todo-Management/ViewModels/Users/UserViewModel.cs
+++ Team-Todo-Management/ViewModels/Users/UserViewModel.cs
@@ -12,6 +12,7 @@ namespace Team_Todo_Management.ViewModels
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Role { get; set; }
         public string FullName {
             get
             {
