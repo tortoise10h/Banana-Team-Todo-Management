9ebb50156995e69a64688396e71fd3286edb6f1c
minhminion <minhminion2015@gmail.com>
2020-12-13 12:56:57 +0700

    [UI] Active link against base url and show User role on top bar

--- Team-Todo-Management/wwwroot/js/site.js
+++ Team-Todo-Management/wwwroot/js/site.js
@@ -1,4 +1,20 @@
 ﻿/** Enable tool tips */
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+
+  var url = window.location.pathname.replace(/\/$/, "");
+  // Create regexp to match current url pathname
+  var urlRegExp = new RegExp(`${url.length > 0 ? url : "/" }|\\?[\\s\\S]{1,}`);
+  // Grab every link from the navigation
+  $(".side-nav li").each(function () {
+      const linkElement = $(this).children()[0];
+      // Test its normalized href against the url pathname regexp
+    if (linkElement) {
+      const href = linkElement.href.replace(window.location.origin, "");
+      const matchString = href.match(urlRegExp)
+      if (matchString && matchString[0] === matchString.input) {
+        $(this).addClass("mm-active");
+      }
+    }
+  });
+});
