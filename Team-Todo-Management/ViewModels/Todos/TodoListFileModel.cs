001cf752733b16167b15f65feed1794d1540a4f9
Nguyen Anh Khoa <nahntvt@gmail.com>
2020-12-14 15:21:38 +0700

    [Feature] upload and dowload file

---
+++ Team-Todo-Management/ViewModels/Todos/TodoListFileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_Todo_Management.ViewModels
+{
+    public class TodoListFileModel
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+    }
+}
