eab21368241c62e6c190d3130ae033376223bb75
minhminion <minhminion2015@gmail.com>
2020-12-14 12:43:02 +0700

    [UI] Re-style Tasks Details Page

--- Team-Todo-Management/wwwroot/js/highlight-status-label-script.js
+++ Team-Todo-Management/wwwroot/js/highlight-status-label-script.js
@@ -3,5 +3,5 @@ $('span[ title="Processing"]').addClass("badge-processing-lighten");
 $('span[ title="Done"]').addClass("badge-done-lighten");
 $('span[ title="Cancelled"]').addClass("badge-cancelled-lighten");
 $('span[ title="Overdue"]').addClass("badge-cancelled-lighten");
-$('span[ title="Public"]').addClass("badge-new-lighten");
-$('span[ title="Private"]').addClass("badge-processing-lighten");
+$('span[ title="Public"]').addClass("badge-public-lighten");
+$('span[ title="Private"]').addClass("badge-private-lighten");
