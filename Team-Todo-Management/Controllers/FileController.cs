8fcdb8da0f08e5408157ecb8095ef6be8b13531a
Nguyen Anh Khoa <nahntvt@gmail.com>
2020-12-15 11:24:18 +0700

    [Feature] delete file

---
+++ Team-Todo-Management/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Team_Todo_Management.Data;
+
+namespace Team_Todo_Management.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly DataContext _context;
+
+        public FileController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> DeleteFile([FromRoute] int id)
+        {
+            var media = await _context.Medias.SingleOrDefaultAsync(x => x.Id == id);
+            if (media == null)
+            {
+                return NotFound();
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), media.Location);
+
+            _context.Medias.Remove(media);
+            await _context.SaveChangesAsync();
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+
+            return RedirectToAction("Details", "Todo", new { id = media.TodoId });
+        }
+    }
+}
